a0143ab05e314cdb16029ef2e16a89879732d743
Dayo <dayonel@hotmail.es>
2024-08-18 12:54:03 +0200

    feat: Add bundle size analyzer.

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);
